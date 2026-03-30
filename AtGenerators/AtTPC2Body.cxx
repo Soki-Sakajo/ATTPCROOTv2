4277b97cd94d824d0e90469f1bb0a5d6550f2e57
soki-sakajo <Soki-Sakajo@users.noreply.github.com>
2026-03-30 11:11:40 +0900

    improved the format for AtTPC2Body.cxx

--- AtGenerators/AtTPC2Body.cxx
+++ AtGenerators/AtTPC2Body.cxx
@@ -488,7 +488,7 @@ Bool_t AtTPC2Body::GenerateReaction(FairPrimaryGenerator *primGen)
          Int_t trackIdCut = 0;
 
          if (!kIsFinalGen) {
-   	   //            trackIdCut = 2; // Remove beam and decaying particle
+   	      //            trackIdCut = 2; // Remove beam and decaying particle
             trackIdCut = 3; // Remove beam and decaying particle
          } else
             trackIdCut = 1; // Remove beam
