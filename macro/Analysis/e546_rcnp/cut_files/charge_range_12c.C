dde96e96c9a719dafcff79beff09bbdf222c91b2
Soki-Sakajo <sakajo@ne.phys.sci.osaka-u.ac.jp>
2026-04-27 12:15:34 +0200

    Making the cut of 12c.

---
+++ macro/Analysis/e546_rcnp/cut_files/charge_range_12c.C
@@ -0,0 +1,24 @@
+{
+//========= Macro generated from object: CUTG/Graph
+//========= by ROOT version6.26/10
+   
+   cutg = new TCutG("CUTG",13);
+   cutg->SetVarX("Charge Range (phi1-phi2-180 < 10 )");
+   cutg->SetVarY("");
+   cutg->SetTitle("Graph");
+   cutg->SetFillStyle(1000);
+   cutg->SetPoint(0,43.4097,59493.7);
+   cutg->SetPoint(1,238.968,364873);
+   cutg->SetPoint(2,348.567,445570);
+   cutg->SetPoint(3,557.02,456646);
+   cutg->SetPoint(4,604.298,548418);
+   cutg->SetPoint(5,361.461,562658);
+   cutg->SetPoint(6,198.138,518354);
+   cutg->SetPoint(7,94.9857,355380);
+   cutg->SetPoint(8,21.9198,102215);
+   cutg->SetPoint(9,15.4728,65822.8);
+   cutg->SetPoint(10,41.2607,61075.9);
+   cutg->SetPoint(11,41.2607,61075.9);
+   cutg->SetPoint(12,43.4097,59493.7);
+   cutg->Draw("");
+}
